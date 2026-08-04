6deec279bd2b0914c0a0571fcc50206e77a027be
MADHUR <madhurgahlot20@gmail.com>
2026-08-04 17:24:48 +0530

    WEEK1

---
+++ WEEK1/Assignments.py
@@ -0,0 +1,51 @@
+'''1.Load a dataset using Pandas and summarize basic stats '''
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("Titanic-Dataset.csv")
+
+print(data)
+data_info = pd.read_csv("Titanic-Dataset.csv").info()
+#print(data_info)
+data_describe = pd.read_csv("Titanic-Dataset.csv").describe()
+#print(data_describe)
+
+# check the missing values
+
+print(f'THE MISSING VALUES : \n {data.isnull()}')
+print(f'THE TOTAL MISSING VALUES: \n {data.isnull().sum()}')
+print(f'TOTAL MISSING VALUE IN WHOLE DATA SETS: \n {data.isnull().sum().sum()}')
+
+#check the values column by column
+
+#print(data.iloc[50:100,11])
+
+data_cleaned = pd.read_csv("Titanic-Dataset.csv")
+
+mode_value = data_cleaned["Embarked"].mode()[0]
+data_cleaned["Embarked"] = data_cleaned["Embarked"].fillna(mode_value)
+
+print(f'After Fill The value of Embarked: \n {data_cleaned.isnull().sum()}')
+
+# now fill the age using mean
+data_cleaned["Age"] = data_cleaned["Age"].fillna(data_cleaned["Age"].mean())
+print(f'The data after filling  the vakue of age: \n {data_cleaned.isnull().sum()}')
+
+# the missing  value of cabin is too high so we drop this table 
+data_cleaned=data_cleaned.drop(columns=["Cabin"])
+print(f'THE DATA AFTER THE DROPING CABIN COLUMN: \n {data_cleaned.isnull().sum()}')
+
+#Now thw data is completly cleaned
+
+le = LabelEncoder()
+
+data_cleaned["Sex"] = le.fit_transform(data_cleaned["Sex"])
+#print(data_cleaned.loc[:50,"Sex"])
+
+encoded_Embarked = pd.get_dummies(data_cleaned, columns=["Embarked"])
+
+print(encoded_Embarked)
+
+
+
+
---
+++ WEEK1/MINIPROJECT1/TITANICSURVIVALCLEANING.py
@@ -0,0 +1,51 @@
+# Mini Project 1: “Titanic Survival Prediction – Data Cleaning Project”
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+# load data
+data = pd.read_csv("D:/BESKILLED/WEEK1/Titanic-Dataset.csv")
+print(data.info())
+print(data.describe())
+print(f'THE NULL VALUE IN DATASET IS \n {data.isnull()}')
+print(f'THE  MISSING VALUE IN EACH COLUMN \n {data.isnull().sum()}')
+print(f'Total Missing Values In The Entire DataSet: \n {data.isnull().sum().sum()}')
+
+#CLEANING THE DATA 
+
+data_cleaned = data.drop(columns=["Cabin"])  # this column have the 687 missing values out of 891
+print(data_cleaned)
+
+# FILL THE DATA : 
+mode_value = data_cleaned["Embarked"].mode()[0]
+data_cleaned["Embarked"] = data_cleaned["Embarked"].fillna(mode_value)
+
+#print(F'THE  COLUMN EMBARKED AFTER FILLING \n {data_cleaned.loc[:50,"Embarked"]}')
+print(f'The Missing value after the filling Embarked \n {data_cleaned.isnull().sum()}')
+
+#Fill the age with the mean
+
+data_cleaned["Age"] = data_cleaned["Age"].fillna(data_cleaned["Age"].mean())
+print(F'THE DATASET AFTER CLEANING \n {data_cleaned.isnull().sum()} ')
+
+# HERE WE WILL ENCODE THE SEX AND THE EMBARKED WITH LABEL ENCODING 
+
+le = LabelEncoder()
+
+data_cleaned["Sex"] = le.fit_transform(data_cleaned["Sex"])
+
+#print(f'THE DATASET AFETR THE SEX ENCODING  \n : {data_cleaned.loc[:50,"Sex"]}')
+
+data_cleaned["Embarked"] = le.fit_transform(data_cleaned["Embarked"])
+
+print(f'THE DATASET AFETR THE ENCODING: \n {data_cleaned.loc[:50,["Sex","Embarked"]]}')
+
+plt.hist(data_cleaned["Age"],bins=20)
+plt.title("THE AGE DISTRIBUTION ")
+plt.xlabel("Age")
+plt.ylabel("Frequency")
+plt.show()
+
+# SAVE THE DATA
+data_cleaned.to_csv("Titanic_Cleaned.csv", index=False)
+
